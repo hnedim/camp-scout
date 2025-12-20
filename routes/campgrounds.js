1b73f68b1b1c5bc4080af9392472d9238686cb9c
hnedim <nhodzic098@gmail.com>
2025-12-20 21:55:53 +0100

    Added review star rating functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,7 +8,6 @@ router.route('/')
     .get(catchAsync(campgrounds.index))
     .post(isLoggedIn, validateCampground, catchAsync(campgrounds.newCampground));
 
-//2.NEW
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
@@ -16,7 +15,6 @@ router.route('/:id')
     .put(isLoggedIn, isAuthor, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-//5.EDIT
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;

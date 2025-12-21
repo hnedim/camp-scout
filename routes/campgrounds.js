2368aae3c1fdda23ac5ea7fa48979621bd3d228f
hnedim <nhodzic098@gmail.com>
2025-12-21 12:49:39 +0100

    Added image upload, delete, and update functionality via Cloudinary/Multer, added carousel sliders on campgrounds, added placholder image fallback

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../utils/middleware.js');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const {storage} = require('../cloudinary');
+const upload = multer({storage});
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.newCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.newCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));

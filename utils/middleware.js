0e4fe37f38a7256309c3f836a5ba428a6eb2a282
hnedim <nhodzic098@gmail.com>
2025-12-25 14:28:11 +0100

    Add style & flash message fix

--- utils/middleware.js
+++ utils/middleware.js
@@ -11,7 +11,7 @@ module.exports.isLoggedIn = (req,res,next) => {
         } else {
             req.session.returnTo = req.get('referrer') || '/campgrounds';
         }
-        req.flash('error', 'you must be signed in')
+        req.flash('error', 'You must be signed in.')
         return res.redirect('/login');
     } else {
         next();
@@ -29,7 +29,7 @@ module.exports.isAuthor = async(req,res,next) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     if(!campground.author.equals(req.user._id)){
-        req.flash('error', 'you are not authorized');
+        req.flash('error', 'You are not authorized.');
         return res.redirect(`/campgrounds/${campground._id}`);
     }
     next();
@@ -39,7 +39,7 @@ module.exports.isReviewAuthor = async(req,res,next) => {
     const {id, reviewId} = req.params;
     const review = await Review.findById(reviewId);
     if(!review.author.equals(req.user._id)){
-        req.flash('error', 'you are not authorized');
+        req.flash('error', 'You are not authorized.');
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
